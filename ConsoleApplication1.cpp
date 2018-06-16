19a1e93deb2e0f424323429c4cdb86dae59418a7
Egorka31 <ego-koto@yandex.ru>
2018-06-16 18:56:32 +0300

    Update ConsoleApplication1.cpp

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -3,168 +3,163 @@
 #include <vector>
 #include <queue>
 #include <algorithm>
-using namespace std;
 
-int n, m, k;
-const int N = 10000;
-vector<int> flow, capacity, c, head, nxt, edge_reverse, edge_ind, edge, parent1, parent2, p, d;
-vector<bool> mark;
-int fre = 0;
-
-void findpath(int current) {
-	mark[current] = true;
-	int ind = head[current];
+struct task {
+	int amount_of_cities, amount_of_roads, amount_of_sons;
+	std::vector<int> flow, capacity, c, head, nxt, edge_reverse, edge_ind, edge, parent1, parent2, potentials, dist_to_node;
+	std::vector<bool> mark;
+	int fre = 0;
+};
+void findpath(int current, task &web) {
+	web.mark[current] = true;
+	int ind = web.head[current];
 	while (ind != -1) {
-		if (!mark[edge[ind]]) {
-			if (flow[ind] == capacity[ind] && capacity[ind] > 0) {
-				parent1[edge[ind]] = current;
-				parent2[edge[ind]] = edge_reverse[ind];
-				findpath(edge[ind]);
+		if (!web.mark[web.edge[ind]]) {
+			if (web.flow[ind] == web.capacity[ind] && web.capacity[ind] > 0) {
+				web.parent1[web.edge[ind]] = current;
+				web.parent2[web.edge[ind]] = web.edge_reverse[ind];
+				findpath(web.edge[ind], web);
 			}
 		}
-		ind = nxt[ind];
+		ind = web.nxt[ind];
 	}
 }
 
-void writepath(int current, int sum) {
-	if (parent1[current] != -1) {
-		writepath(parent1[current], sum + 1);
-		flow[parent2[current]] -= 1;
-		int ind_norm = edge_reverse[parent2[current]];
-		flow[ind_norm] += 1;
-		cout << edge_ind[parent2[current]] << " ";
+void writepath(int current, int sum, task &web) {
+	if (web.parent1[current] != -1) {
+		writepath(web.parent1[current], sum + 1, web);
+		web.flow[web.parent2[current]] -= 1;
+		int ind_norm = web.edge_reverse[web.parent2[current]];
+		web.flow[ind_norm] += 1;
+		std::cout << web.edge_ind[web.parent2[current]] << " ";
 		return;
 	}
-	cout << sum << endl;
+	std::cout << sum << std::endl;
 }
 
-bool count_dist() {
-	for (int i = 0; i < n; ++i) {
-		p[i] = d[i];
-		d[i] = 1e9;
-		parent1[i] = -1;
-		parent2[i] = -1;
+bool count_dist(task& web) {
+	for (size_t i = 0; i < web.amount_of_cities; ++i) {
+		web.potentials[i] = web.dist_to_node[i];
+		web.dist_to_node[i] = 1e9;
+		web.parent1[i] = -1;
+		web.parent2[i] = -1;
 	}
-	d[0] = 0;
-	priority_queue<pair<int, int>> q;
-	q.push(make_pair(0, 0));
-	while (!q.empty()) {
-		int v = q.top().second;
-		int dist = -q.top().first;
-		q.pop();
-		if (d[v] == dist) {
-			int ind = head[v];
+	web.dist_to_node[0] = 0;
+	std::priority_queue<std::pair<int, int>> distance_queue;
+	distance_queue.push(std::make_pair(0, 0));
+	while (!distance_queue.empty()) {
+		int v = distance_queue.top().second;
+		int dist = -distance_queue.top().first;
+		distance_queue.pop();
+		if (web.dist_to_node[v] == dist) {
+			int ind = web.head[v];
 			while (ind != -1) {
-				if (flow[ind] < capacity[ind]) {
-					int dist_to = d[v] + c[ind] + p[v] - p[edge[ind]];
-					if (dist_to < d[edge[ind]]) {
-						d[edge[ind]] = dist_to;
-						parent1[edge[ind]] = v;
-						parent2[edge[ind]] = edge_reverse[ind];
-						q.push(make_pair(-dist_to, edge[ind]));
+				if (web.flow[ind] < web.capacity[ind]) {
+					int dist_to = web.dist_to_node[v] + web.c[ind] + web.potentials[v] - web.potentials[web.edge[ind]];
+					if (dist_to < web.dist_to_node[web.edge[ind]]) {
+						web.dist_to_node[web.edge[ind]] = dist_to;
+						web.parent1[web.edge[ind]] = v;
+						web.parent2[web.edge[ind]] = web.edge_reverse[ind];
+						distance_queue.push(std::make_pair(-dist_to, web.edge[ind]));
 					}
 				}
-				ind = nxt[ind];
+				ind = web.nxt[ind];
 			}
 		}
 	}
-	return (d[n - 1] != 1e9);
+	return (web.dist_to_node[web.amount_of_cities - 1] != 1e9);
 }
 
-int upd(int v) {
+int upd(int v, task &web) {
 	int cur_c = 0;
-	while (parent1[v] != -1) {
-		cur_c += c[edge_reverse[parent2[v]]];
-		flow[edge_reverse[parent2[v]]]++;
-		flow[parent2[v]]--;
+	while (web.parent1[v] != -1) {
+		cur_c += web.c[web.edge_reverse[web.parent2[v]]];
+		web.flow[web.edge_reverse[web.parent2[v]]]++;
+		web.flow[web.parent2[v]]--;
 
-		v = parent1[v];
+		v = web.parent1[v];
 	}
 
 	return cur_c;
 }
 
-int main() {
-	cin >> n >> m >> k;
-
-	flow.resize(N);
-	capacity.resize(N);
-	c.resize(N);
-	head.resize(N);
-	nxt.resize(N);
-	edge_reverse.resize(N);
-	edge_ind.resize(N);
-	edge.resize(N);
-	mark.resize(N);
-	parent1.resize(N);
-	parent2.resize(N);
-	p.resize(N);
-	d.resize(N);
-	for (int i = 0; i < n; i++) {
-		head[i] = -1;
-	}
-
-	for (int i = 0; i < m; i++) {
-		int x, y, w;
-		cin >> x >> y >> w;
-
-		edge[fre] = y - 1;
-		c[fre] = w;
-		capacity[fre] = 1;
-		edge_ind[fre] = i + 1;
-		edge_reverse[fre] = fre + 1;
-		nxt[fre] = head[x - 1];
-		head[x - 1] = fre;
-		++fre;
-
-		edge[fre] = x - 1;
-		c[fre] = -w;
-		edge_ind[fre] = i + 1;
-		edge_reverse[fre] = fre - 1;
-		nxt[fre] = head[y - 1];
-		head[y - 1] = fre;
-		++fre;
-
-		edge[fre] = x - 1;
-		c[fre] = w;
-		capacity[fre] = 1;
-		edge_ind[fre] = i + 1;
-		edge_reverse[fre] = fre + 1;
-		nxt[fre] = head[y - 1];
-		head[y - 1] = fre;
-		++fre;
-
-		edge[fre] = y - 1;
-		c[fre] = -w;
-		edge_ind[fre] = i + 1;
-		edge_reverse[fre] = fre - 1;
-		nxt[fre] = head[x - 1];
-		head[x - 1] = fre;
-		++fre;
-	}
-
-	int f = 0, sum = 0;
-	while (count_dist()) {
-		sum += upd(n - 1);
-		++f;
-		if (f == k)
-			break;
-	}
-	if (f < k) {
-		cout << -1 << endl;
+void padding(int x, int w, int y, bool tpr, int i, task &web) {
+	web.edge[web.fre] = x - 1;
+	web.c[web.fre] = w;
+	if (tpr) {
+		web.capacity[web.fre] = 1;
+		web.edge_reverse[web.fre] = web.fre + 1;
 	}
 	else {
-		cout << fixed << setprecision(6) << sum * 1.0 / k << endl;
-		for (int i = 0; i < k; ++i) {
-			for (int j = 0; j < n; ++j) {
-				mark[j] = false;
-				parent1[j] = -1;
-				parent2[j] = -1;
+		web.edge_reverse[web.fre] = web.fre - 1;
+	}
+	web.edge_ind[web.fre] = i + 1;
+	web.nxt[web.fre] = web.head[y - 1];
+	web.head[y - 1] = web.fre;
+	++web.fre;
+}
+
+void read(task &web) {
+	std::cin >> web.amount_of_cities >> web.amount_of_roads >> web.amount_of_sons;
+
+	web.flow.resize(10000);
+	web.capacity.resize(10000);
+	web.c.resize(10000);
+	web.head.resize(10000);
+	web.nxt.resize(10000);
+	web.edge_reverse.resize(10000);
+	web.edge_ind.resize(10000);
+	web.edge.resize(10000);
+	web.mark.resize(10000);
+	web.parent1.resize(10000);
+	web.parent2.resize(10000);
+	web.potentials.resize(10000);
+	web.dist_to_node.resize(10000);
+
+	for (size_t i = 0; i < web.amount_of_cities; i++) {
+		web.head[i] = -1;
+	}
+
+	for (size_t i = 0; i < web.amount_of_roads; i++) {
+		int x, y, w;
+		std::cin >> x >> y >> w;
+		padding(y, w, x, true, i, web);
+		padding(x, -w, y, false, i, web);
+		padding(x, w, y, true, i, web);
+		padding(y, -w, x, false, i, web);
+	}
+}
+void solution(int &sum, int &f, task &web) {
+	while (count_dist(web)) {
+		sum += upd(web.amount_of_cities - 1, web);
+		++f;
+		if (f == web.amount_of_sons)
+			break;
+	}
+}
+void output(int &f, task &web, int &sum) {
+	if (f < web.amount_of_sons) {
+		std::cout << -1 << std::endl;
+	}
+	else {
+		std::cout << std::fixed << std::setprecision(6) << sum * 1.0 / web.amount_of_sons << std::endl;
+		for (size_t i = 0; i < web.amount_of_sons; ++i) {
+			for (size_t j = 0; j < web.amount_of_cities; ++j) {
+				web.mark[j] = false;
+				web.parent1[j] = -1;
+				web.parent2[j] = -1;
 			}
-			findpath(0);
-			writepath(n - 1, 0);
-			cout << endl;
+			findpath(0, web);
+			writepath(web.amount_of_cities - 1, 0, web);
+			std::cout << std::endl;
 		}
 	}
+}
+int main() {
+	task web;
+	read(web);
+	int f = 0, sum = 0;
+	solution(sum, f, web);
+	output(f, web, sum);
 	return 0;
 }
